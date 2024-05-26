f35b9cd8b8c8d6476c951b806326443adea0a546
Mccranky83 <ericcai20021118@gmail.com>
2024-05-26 14:23:47 +0800

    Align shell behavior of FloatTerm plugin
    `ps -p $$ -o comm=` outputs '/bin/zsh' in FloatTerm's new shell, as
    opposed to '-zsh' in a regular new shell.

--- functions/generate_shell_indicator.zsh
+++ functions/generate_shell_indicator.zsh
@@ -1,8 +1,7 @@
 generate_shell_indicator() {
 	local shell_name="$(
-		IFS='-'
-		list=($(ps -p $$ -o comm=))
-		echo ${list[@]}
+		process_name="$(ps -p $$ -o comm=)"
+		echo "${process_name##*[^[:alpha:]]}"
 	)"
 	local max_length=8
 	printf -v shell_indicator "%s%*s" "$shell_name" $((max_length - ${#shell_name}))
